f38e180df68775b9e608cd677022af28dc4e9fc3
Samuel Bryan <remotepilotsam@gmail.com>
2022-06-27 20:55:47 -0400

    Added comments to describe the code

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,24 +1,28 @@
+//Samuel Bryan
+//CIS 1202 800
+//June 27th, 2022
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-float average(int n1, int n2, int n3)
+float average(int n1, int n2, int n3) //3 numbers from user input
 {
-	float avg = (n1 + n2 + n3) / 3.0f;
-	return avg;
+	float avg = (n1 + n2 + n3) / 3.0f; //Calculates average of user input
+	return avg; //returns the average
 }
 
 int main()
 {
-	float n1 = 0, n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n3;
+	float n1 = 0, n2 = 0, n3 = 0; //Initial values prior to user input
+	cout << "This program calculates the average of three numbers." << endl; //Describes program
+ 	cout << "First number: "; cin >> n1; //First input
+	cout << "Second number: "; cin >> n2; //Second input
+	cout << "Third number: "; cin >> n3; //Third input
 
-	float a = average(n1, n2, n3);
+	float a = average(n1, n2, n3); //Calls function to calculate average of user input
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(1) << fixed << "The average is " << a << endl; //Displays average
 
 	system("pause");
 	return 0;
